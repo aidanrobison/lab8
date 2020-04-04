11d27d143ac9ccab5642c0ab932bf67405292286
Aidan Robison <aidangrobison@gmail.com>
2020-04-04 02:40:37 +0000

    Created the perl file

---
+++ perl/perl.pl
@@ -0,0 +1,33 @@
+#!/usr/local/bin/perl
+
+$count = 0;
+while (<stdin>) {
+    @w = split;
+    $count++;
+    for ($i=0; $i<=$#w; $i++) {
+	$s[$i] += $w[$i];
+    }
+}
+
+for ($i=0; $i<=$#w; $i++) {
+    print $s[$i]/$count, "\t";
+}
+
+print "\n";
+#!/usr/local/bin/perl
+
+$count = 0;
+while (<stdin>) {
+    @w = split;
+    $count++;
+    for ($i=0; $i<=$#w; $i++) {
+	$s[$i] += $w[$i];
+    }
+}
+
+for ($i=0; $i<=$#w; $i++) {
+    print $s[$i]/$count, "\t";
+}
+
+print "\n";
+
